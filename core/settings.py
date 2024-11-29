e7effbd5a8181a1578b0879da346a29f73f44ae6
dlcastra <1dlcastra@gmail.com>
2024-11-29 19:43:16 +0100

    empty base structure

--- core/settings.py
+++ core/settings.py
@@ -31,6 +31,11 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # CUSTOM APPS
+    "users.apps.UsersConfig",
+    "tasks.apps.TasksConfig",
+    "comments.apps.CommentsConfig"
+    # BASE
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
---
+++ users/models.py
@@ -0,0 +1,18 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class CustomUser(AbstractUser):
+    """
+    Admin
+    Client
+    Assignee
+    """
+    ...
+
+
+class CustomAuthToken(models.Model):
+    """
+    JWT
+    """
+    ...
