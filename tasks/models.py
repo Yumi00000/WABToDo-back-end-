96f55ac57e48e6a3e216686f90b8153906dd3917
dlcastra <1dlcastra@gmail.com>
2024-12-03 13:03:29 +0100

    Models for user management, authentication and authorization

--- tasks/models.py
+++ tasks/models.py
@@ -1,5 +1,15 @@
 from django.db import models
 
+from core import settings
+from users.models import Team
 
-# class Task(models.Model):
-#     ...
+
+class Task(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    executor = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True, related_name="tasks_as_executor"
+    )
+    team = models.ForeignKey(Team, on_delete=models.CASCADE, related_name="tasks")
+    status = models.CharField(max_length=11, default="pending")
+    deadline = models.DateField(null=True, blank=True)
--- users/models.py
+++ users/models.py
@@ -1,18 +1,58 @@
+from datetime import datetime, timedelta
+
+import jwt
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+from core import settings
 
-# class CustomUser(AbstractUser):
-#     """
-#     Admin
-#     Client
-#     Assignee
-#     """
-#     ...
-#
-#
-# class CustomAuthToken(models.Model):
-#     """
-#     JWT
-#     """
-#     ...
+
+class CustomUser(AbstractUser):
+    """
+    Admin
+    Client
+    Executor
+    """
+
+    phone_number = models.CharField(max_length=15, unique=True, null=True, blank=True)
+    is_team_member = models.BooleanField(default=False)
+    is_admin = models.BooleanField(default=False)
+
+
+class Team(models.Model):
+    leader = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name="team_leader")
+    members = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name="team_as_member")
+    status = models.CharField(max_length=11, default="available")
+
+
+class CustomAuthToken(models.Model):
+    """JWT"""
+
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name="user_auth_token")
+    key = models.CharField(max_length=512, unique=True)
+    user_agent = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    expires_at = models.DateTimeField()
+
+    def save(self, *args, **kwargs):
+        if not self.key:
+            self.key = self.generate_jwt()
+        if not self.expires_at:
+            self.expires_at = datetime.now() + timedelta(hours=168)
+        return super().save(*args, **kwargs)
+
+    def generate_jwt(self):
+        payload = {
+            "user_id": self.user.id,
+            "username": self.user.username,
+            "iat": datetime.now(),
+            "exp": datetime.now() + timedelta(hours=168),
+            "user_agent": self.user_agent,
+        }
+        return jwt.encode(payload, settings.SECRET_KEY, algorithm="HS256")
+
+    def is_valid(self):
+        return self.expires_at > datetime.now()
+
+    def __str__(self):
+        return f"Token owner {self.user.username} - token: {self.key}"
