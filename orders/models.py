3508a92aab58cb9754af2d895edfe4149f3a7299
dlcastra <1dlcastra@gmail.com>
2024-12-03 14:10:29 +0100

    Order model

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,18 @@
+from core import settings
 from django.db import models
 
-# Create your models here.
+from tasks.models import Task
+from users.models import Team
+
+
+class Order(models.Model):
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="client_orders")
+    name = models.CharField(max_length=128)
+    description = models.TextField()
+    accepted = models.BooleanField(default=False)
+    team = models.ForeignKey(Team, on_delete=models.CASCADE, related_name="team_orders", null=True, blank=True)
+    tasks = models.ForeignKey(Task, on_delete=models.CASCADE, related_name="task_orders", null=True, blank=True)
+    deadline = models.DateField()
+    createdAt = models.DateTimeField(auto_now_add=True, blank=True)
+    updatedAt = models.DateTimeField(null=True, blank=True)
+    acceptedAt = models.DateTimeField(null=True, blank=True)
