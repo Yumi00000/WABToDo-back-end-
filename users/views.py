03e4f2d1782c072878662eb3b6e2deeb24ff148f
dlcastra <1dlcastra@gmail.com>
2024-12-05 16:08:31 +0100

    Dashboard view

--- users/views.py
+++ users/views.py
@@ -1,8 +1,10 @@
+from django.db.models import Q
 from rest_framework import generics, permissions, status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.viewsets import GenericViewSet
 
+from orders.models import Order
 from users import serializers
 from users.models import CustomUser, CustomAuthToken
 
@@ -33,3 +35,16 @@ class LoginView(APIView):
             return Response({"token": token.key}, status=status.HTTP_200_OK)
 
         return Response({"token": token.key}, status=status.HTTP_201_CREATED)
+
+
+class DashboardView(generics.ListAPIView, GenericViewSet):
+    permission_classes = [permissions.IsAuthenticated]
+    serializer_class = serializers.DashboardSerializer
+
+    def get_queryset(self):
+        user = self.request.user
+        owner_orders = Q(owner=user)
+        team_orders = Q(team__members__user=user)
+        queryset = Order.objects.filter(owner_orders | team_orders).distinct()
+
+        return queryset
