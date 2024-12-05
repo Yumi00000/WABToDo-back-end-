3daff0266df956509038f4b6bf8837b7d2751982
dlcastra <1dlcastra@gmail.com>
2024-12-05 16:08:17 +0100

    View for order creating

--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions, status
+from rest_framework.response import Response
+from rest_framework.viewsets import GenericViewSet
 
-# Create your views here.
+from orders.models import Order
+from orders import serializers
+
+
+class CreateOrderView(generics.CreateAPIView, GenericViewSet):
+    queryset = Order.objects.all()
+    permission_classes = [permissions.IsAuthenticated]
+    serializer_class = serializers.CreateOrderSerializer
