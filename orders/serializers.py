fba0368c71af743387a3596c3013e73a70aa1f7a
dlcastra <1dlcastra@gmail.com>
2024-12-05 16:06:30 +0100

    Base order serializer and serializer for creating order

---
+++ orders/serializers.py
@@ -0,0 +1,48 @@
+from rest_framework import serializers
+
+from orders.models import Order
+from tasks.models import Task
+from tasks.serializers import TaskSerializer
+
+
+class OrderSerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source="owner.id")
+    accepted = serializers.ReadOnlyField()
+    team = serializers.ReadOnlyField(source="team.id", default=None)
+    tasks = serializers.SerializerMethodField()
+    createdAt = serializers.ReadOnlyField(source="created_at")
+    updatedAt = serializers.ReadOnlyField(source="updated_at")
+    acceptedAt = serializers.ReadOnlyField(source="accepted_at")
+
+    class Meta:
+        model = Order
+        fields = [
+            "id",
+            "owner",
+            "name",
+            "description",
+            "deadline",
+            "createdAt",
+            "updatedAt",
+            "acceptedAt",
+            "accepted",
+            "team",
+            "tasks",
+        ]
+
+    def get_tasks(self, obj):
+        tasks = Task.objects.filter(order=obj)
+        return TaskSerializer(tasks, many=True).data
+
+
+class CreateOrderSerializer(OrderSerializer):
+    def create(self, validated_data):
+        user = self.context["request"].user.id
+        order = Order.objects.create(
+            owner_id=user,
+            name=validated_data["name"],
+            description=validated_data["description"],
+            deadline=validated_data["deadline"],
+        )
+        order.save()
+        return order
