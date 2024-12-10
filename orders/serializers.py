d3880d493e79eee03d81246d5b3bd7e2dabc845b
dlcastra <1dlcastra@gmail.com>
2024-12-10 13:26:07 +0100

    Added serializers for order management

--- orders/serializers.py
+++ orders/serializers.py
@@ -1,8 +1,11 @@
+from django.utils import timezone
 from rest_framework import serializers
 
 from orders.models import Order
+from orders.utils import change_date_format
 from tasks.models import Task
 from tasks.serializers import TaskSerializer
+from users.models import Team
 
 
 class OrderSerializer(serializers.ModelSerializer):
@@ -28,6 +31,7 @@ class OrderSerializer(serializers.ModelSerializer):
             "accepted",
             "team",
             "tasks",
+            "status",
         ]
 
     def get_tasks(self, obj):
@@ -70,3 +74,80 @@ class UpdateOrderSerializer(OrderSerializer):
     name = serializers.CharField(required=False)
     description = serializers.CharField(required=False)
     deadline = serializers.DateField(required=False)
+
+
+class UnacceptedOrderSerializer(OrderSerializer):
+    def to_representation(self, instance: Order):
+        created_at = change_date_format(instance.created_at)
+
+        return {
+            "id": instance.id,
+            "name": instance.name,
+            "description": instance.description,
+            "deadline": instance.deadline,
+            "createdAt": created_at,
+            "status": instance.status,
+        }
+
+
+class AcceptOrderSerializer(serializers.ModelSerializer):
+    accepted = serializers.BooleanField(required=True)
+    team = serializers.IntegerField(source="team.id", required=True)
+    status = serializers.CharField()
+
+    class Meta:
+        model = Order
+        fields = ["accepted", "team", "status"]
+
+    def validate(self, attrs):
+        required_fields = ["accepted", "team"]
+        missing_fields = [field for field in required_fields if field not in attrs]
+
+        try:
+            team = attrs["team"]
+            status = attrs["status"]
+            team_instance = Team.objects.get(id=team["id"])
+            if status == "active" and team_instance.status == "unavailable":
+                raise serializers.ValidationError({"message": "This team is currently unavailable."})
+
+        except Team.DoesNotExist:
+            raise serializers.ValidationError({"error": "This team does not exist."})
+
+        if missing_fields:
+            raise serializers.ValidationError({"missing_fields": f"Fields {missing_fields} are required."})
+
+        return attrs
+
+    def update(self, instance: Order, validated_data):
+        is_accepted = validated_data.get("accepted", False)
+        status = validated_data.get("status", None)
+        print(status)
+        team = validated_data.get("team", None)
+        team_instance = Team.objects.get(id=team["id"])
+
+        if is_accepted and status == "active":
+            instance.accepted = True
+            instance.accepted_at = timezone.now()
+            instance.team_id = team["id"]
+            instance.status = status
+            team_instance.status = "unavailable"
+            team_instance.save()
+
+        instance.status = status
+        team_instance.status = "available"
+
+        instance.save()
+        team_instance.save()
+        return instance
+
+    def to_representation(self, instance: Order):
+        accepted_at = change_date_format(instance.accepted_at)
+
+        return {
+            "id": instance.id,
+            "name": instance.name,
+            "accepted": instance.accepted,
+            "acceptedAt": accepted_at,
+            "team": instance.team_id,
+            "status": instance.status,
+        }
