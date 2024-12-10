bfa6e2d61ed03f42535974efbe256a40235850b3
dlcastra <1dlcastra@gmail.com>
2024-12-10 13:28:08 +0100

    views for order management

--- orders/views.py
+++ orders/views.py
@@ -1,11 +1,9 @@
-from rest_framework import generics, permissions, status
-from rest_framework.response import Response
-from rest_framework.views import APIView
+from rest_framework import generics, permissions
 from rest_framework.viewsets import GenericViewSet
 
 from core import permissions as custom_perm
-from orders.models import Order
 from orders import serializers
+from orders.models import Order
 
 
 class CreateOrderView(generics.CreateAPIView, GenericViewSet):
@@ -18,3 +16,15 @@ class EditOrderView(generics.UpdateAPIView, GenericViewSet):
     queryset = Order.objects.all()
     permission_classes = [custom_perm.IsOrderOwnerOrAdmin]
     serializer_class = serializers.UpdateOrderSerializer
+
+
+class GetUnacceptedOrdersView(generics.ListAPIView, GenericViewSet):
+    queryset = Order.objects.filter(accepted=False, status="active")
+    permission_classes = [custom_perm.IsAdminOrStaff]
+    serializer_class = serializers.UnacceptedOrderSerializer
+
+
+class OrderManagementView(generics.UpdateAPIView, GenericViewSet):
+    queryset = Order.objects.filter(status="active")
+    permission_classes = [custom_perm.IsAdminOrStaff]
+    serializer_class = serializers.AcceptOrderSerializer
