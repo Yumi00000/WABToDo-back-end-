421eb9202a78fb5af2d7fd1e0529ac2886665323
dlcastra <1dlcastra@gmail.com>
2024-12-10 13:25:37 +0100

    New permission witch allows access only to admin and staff users like managers

--- core/permissions.py
+++ core/permissions.py
@@ -2,6 +2,10 @@ from rest_framework import permissions
 
 
 class IsOrderOwnerOrAdmin(permissions.BasePermission):
+    """
+    Allow access only to order owner or admin
+    """
+
     def has_object_permission(self, request, view, obj):
         if request.user and request.user.is_staff:
             return True
@@ -10,3 +14,12 @@ class IsOrderOwnerOrAdmin(permissions.BasePermission):
             return True
 
         return False
+
+
+class IsAdminOrStaff(permissions.BasePermission):
+    """
+    Allow access only to admin and staff users like managers
+    """
+
+    def has_permission(self, request, view):
+        return bool(request.user and (request.user.is_staff or request.user.is_admin))
