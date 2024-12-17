e397d3f4f9962b33741406a55e7b41a1dd6c2700
dlcastra <1dlcastra@gmail.com>
2024-12-17 13:21:07 +0100

    Added a function to retrieve all team tasks, as well as filtering and pagination for this function

--- tasks/urls.py
+++ tasks/urls.py
@@ -3,7 +3,7 @@ from rest_framework import routers
 from tasks import views
 
 router = routers.DefaultRouter()
-# router.register(r"", views.GetTeamTasksView, basename="tasks")
+router.register(r"", views.GetTeamTasksView, basename="tasks")
 router.register(r"create", views.CreateTaskView, basename="create-task")
 router.register(r"edit", views.UpdateTaskView, basename="edit-task")
 
