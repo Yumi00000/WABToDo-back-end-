d4809b925079b1dc9a26277313cae1faf3fdb2f2
Yumi <vb34550@gmail.com>
2024-12-23 18:45:47 +0100

    fixed merge conflicts

--- users/views.py
+++ users/views.py
@@ -7,7 +7,6 @@ from allauth.socialaccount.providers.oauth2.client import OAuth2Client
 from dj_rest_auth.registration.views import SocialLoginView
 from django.db.models import Q
 from rest_framework import generics, permissions, status
-from rest_framework.authentication import SessionAuthentication
 from rest_framework.response import Response
 from rest_framework.reverse import reverse
 from rest_framework.views import APIView
@@ -17,64 +16,6 @@ from core import permissions as c_prm, settings
 from orders.models import Order
 from users import serializers
 from users.models import CustomAuthToken, Team
-
-
-####?????####Maybe can be removed####?????####
-
-# class RegistrationView(generics.CreateAPIView, GenericViewSet):
-#     queryset = CustomUser.objects.all()
-#     permission_classes = [permissions.AllowAny]
-#     serializer_class = serializers.RegistrationSerializer
-#
-#     def create(self, request, *args, **kwargs):
-#         serializer = self.get_serializer(data=request.data)
-#
-#         if serializer.is_valid():
-#             serializer.save(request=request)
-#         return Response({"detail": "User registered successfully!"}, status=201)
-
-
-####?????####Maybe can be removed####?????####
-
-# class LoginView(APIView):
-#     permission_classes = [permissions.AllowAny]
-#     serializer_class = serializers.CustomLoginSerializer
-#
-#     def post(self, request, *args, **kwargs):
-#         user_agent = request.META.get("HTTP_USER_AGENT", "Unknown")
-#
-#         serializer = self.serializer_class(data={**request.data, "user_agent": user_agent})
-#         if not serializer.is_valid():
-#             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-#
-#         user = serializer.validated_data["user"]
-#         token, created = CustomAuthToken.objects.get_or_create(
-#             user=user,
-#             user_agent=user_agent,
-#         )
-#         if token and token.is_valid():
-#             return Response({"token": token.key}, status=status.HTTP_200_OK)
-#
-#         return Response({"token": token.key}, status=status.HTTP_201_CREATED)
-
-####?????####Maybe can be removed####?????####
-# class LogoutView(APIView):
-#     permission_classes = [permissions.IsAuthenticated]
-#     logger = logging.getLogger()
-#
-#     def post(self, request):
-#         user_agent = request.META.get("HTTP_USER_AGENT", "Unknown")
-#         try:
-#             token = CustomAuthToken.objects.get(user=request.user, user_agent=user_agent)
-#             token.status = "Offline"
-#             token.save()
-#
-#             self.logger.info("The user logged out successfully")
-#             return Response({"detail": "Successfully logged out."}, status=status.HTTP_200_OK)
-#
-#         except CustomAuthToken.DoesNotExist:
-#             self.logger.error("Invalid token")
-#             return Response({"detail": "Token not found."}, status=status.HTTP_404_NOT_FOUND)
 
 
 class GoogleLoginView(SocialLoginView):
