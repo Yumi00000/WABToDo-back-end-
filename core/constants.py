9012b9ea74271c47d8809428e7576c8332badf85
dlcastra <1dlcastra@gmail.com>
2025-01-04 19:00:01 +0100

    Added constant with fake data for testing

---
+++ core/constants.py
@@ -0,0 +1,12 @@
+ORDER_FAKE_CREATING_DATA = {
+    "id": 2,
+    "name": "TestOrderName",
+    "description": (
+        "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. "
+        "Aenean commodo ligula eget dolor. Aenean massa.\n"
+        "Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.\n"
+        "Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem.\n"
+        "Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate."
+    ),
+    "deadline": "2025-12-31",
+}
